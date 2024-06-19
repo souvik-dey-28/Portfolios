1dc58e2cf0b07f94ba4c500f40ec72a61e632e44
Souvik Dey <117342010+souvik-dey-28@users.noreply.github.com>
2024-06-19 09:57:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// loader script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Simulate an API request or any async operation
+    setTimeout(() => {
+        hideLoader();
+        showContent();
+    }, 3000); // Replace with your actual data loading logic and time
+
+    function hideLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+    }
+
+    function showContent() {
+        const content = document.getElementById("content");
+        content.style.display = "block";
+    }
+});
+
+
+
+// right click disable code????
+
+
+document.oncontextmenu = document.body.oncontextmenu = function() {
+  return false;
+};
